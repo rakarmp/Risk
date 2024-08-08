d925c816616d4da8bf72a9b138f02673dd1dce3b
rakarmp <raka_088@smkassalaambandung.sch.id>
2024-08-09 00:46:00 +0700

    090824 - little trick enable secirity settings

---
+++ service.sh
@@ -0,0 +1,19 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+wait_until_login() {
+while [[ `getprop sys.boot_completed` -ne 1 && -d "/sdcard" ]]
+do
+sleep 2
+done
+local test_file="/sdcard/.PERMISSION_TEST"
+touch "$test_file"
+while [ ! -f "$test_file" ]; do
+touch "$test_file"
+sleep 2
+done
+rm "$test_file"
+}
+
+wait_until_login
+sleep 5
+setprop persist.security.adbinput 1
